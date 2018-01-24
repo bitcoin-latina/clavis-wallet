f9e8368cf1a2cdf34d83bf3d6b04cd8227c22763
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-01-24 14:52:37 -0500

    Removed Outdated and Unstable 'Builds' Folder

---
+++ src/main/java/init/build/Setup_Web3.java
@@ -0,0 +1,45 @@
+package init.build;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.ipc.UnixIpcService;
+import org.web3j.protocol.ipc.WindowsIpcService;
+import ui.Global;
+import utils.Toolkit;
+
+import java.io.File;
+
+public class Setup_Web3 {
+    Web3j web3j;
+    Admin geth;
+    String macIPC=File.separator + "BCL_Node" + File.separator + "geth.ipc";
+    String macIPCFull=Global.getPath()+File.separator + "BCL_Node" + File.separator + "geth.ipc";
+    String windowsIPC="\\\\.\\pipe\\geth.ipc";
+
+    public void setupWeb3(){
+        if(Global.getOS().contains("win")){
+            while(Toolkit.Is_Empty_File(windowsIPC)){
+                //Do Nothing But Sleep
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Global.setWeb3j(Web3j.build(new WindowsIpcService(windowsIPC)));
+            Global.setGeth(Admin.build(new WindowsIpcService(windowsIPC)));
+        }
+        if(Global.getOS().contains("mac")){
+            while(Toolkit.Is_Empty_File(macIPC)){
+                //Do Nothing But Sleep
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Global.setWeb3j(Web3j.build(new UnixIpcService(macIPCFull)));
+            Global.setGeth(Admin.build(new UnixIpcService(macIPCFull)));
+        }
+    }
+}
